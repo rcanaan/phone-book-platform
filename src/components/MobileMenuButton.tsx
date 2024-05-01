99c959e3f9c39706d80a6c0f14056f1620d2bb1d
Rinat Canaan <58044154+rcanaan@users.noreply.github.com>
2024-05-01 13:45:23 +0300

    header button adjusment

--- src/components/MobileMenuButton.tsx
+++ src/components/MobileMenuButton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function MobileMenuButton() {
   return (
-    <div className="h-10 w-16 bg-gray-300 flex justify-center items-center">
+    <div className="h-12 w-16 bg-gray-300 flex justify-center items-center">
       <div className="flex flex-col h-4 w-4 justify-between items-center">
         <div className="w-full h-[0.2rem] bg-black rounded"></div>
         <div className="w-full h-[0.2rem] bg-black rounded"></div>
--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,23 +1,34 @@
 import { Link } from "react-router-dom";
 import MobileMenuButton from "../components/MobileMenuButton";
+import { useState } from "react";
+import clsx from "clsx";
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(true);
+  const handleClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <>
-      <nav className="bg-gray-800 relative text-white lg:h-14">
+      <nav
+        className={clsx("bg-gray-800 relative text-white lg:h-14", {
+          "h-12": isMenuOpen,
+        })}
+      >
         <ul className="flex flex-col space-y-4 px-6 pt-4 pb-2  lg:flex-row lg:space-x-10 lg:space-y-0 ">
           <li>
             <Link to="/">Home</Link>
           </li>
-          <li>
-            <Link to="/addContact">Add Contact</Link>
-          </li>
-          <li>
-            <Link to="/contactList">Contact List</Link>
-          </li>
-
+          <div className={clsx("space-y-4", { collapse: isMenuOpen })}>
+            <li>
+              <Link to="/addContact">Add Contact</Link>
+            </li>
+            <li>
+              <Link to="/contactList">Contact List</Link>
+            </li>
+          </div>
           <li className="lg:hidden">
-            <button className="absolute right-0 top-0">
+            <button onClick={handleClick} className="absolute right-0 top-0">
               <MobileMenuButton />
             </button>
           </li>
